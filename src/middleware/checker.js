4924e274facc7ce0cce850dfc795aaf5ca8cb3e4
heeinso <heeinso@snu.ac.kr>
2018-09-21 15:23:15 +0900

    Fix export error

--- src/middleware/checker.js
+++ src/middleware/checker.js
@@ -1,12 +1,14 @@
-import {ANSWER_QUESTION} from '../constants/actionType';
+import { ANSWER_QUESTION } from '../constants/actionType';
 
-export default const checker = store => next => action => {
-    if (action.type === ANSWER_QUESTION) {
-        const users = store.getState().users;
-        const answers = Object.keys(users[action.authedUser].answers);
-        if (answer.indexOf(action.qid) > -1) {
-            return alert('You have already answered this question!');
-        }
-    }
-    return next(action);
-}
+const checker = store => next => action => {
+	if (action.type === ANSWER_QUESTION) {
+		const users = store.getState().users;
+		const answers = Object.keys(users[action.authedUser].answers);
+		if (answers.indexOf(action.qid) > -1) {
+			return alert('You have already answered this question!');
+		}
+	}
+	return next(action);
+};
+
+export default checker;
--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,8 +1,10 @@
-export default const logger = store => next => action => {
-    console.group(action.type);
-        console.log('The action: ', action);
-        const result = next(action);
-        console.log('The new state is: ', store.getState());
-    console.groupEnd();
-    return result;
-}
+const logger = store => next => action => {
+	console.group(action.type);
+	console.log('The action: ', action);
+	const result = next(action);
+	console.log('The new state is: ', store.getState());
+	console.groupEnd();
+	return result;
+};
+
+export default logger;
