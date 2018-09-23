0a6757841b95577616aa12d72f9bc95011e6c903
heeinso <heeinso@snu.ac.kr>
2018-09-23 14:46:12 +0900

    Fix Navigation bar active styling error

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,74 +1,91 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { Avatar, Layout, Menu } from 'antd';
 
 const { Header } = Layout;
 
-const Navbar = props => {
-	const { authedUser, users } = props;
-	const avatar = authedUser
-		? users[authedUser].avatarURL
-		: 'https://i.imgur.com/dGo8DOk.jpg';
+class Navbar extends Component {
+	state = {
+		current: '/',
+	};
 
-	const loggedIn = authedUser !== null;
+	handleClick = () => {
+		this.setState({
+			current: window.location.pathname,
+		});
+	};
 
-	return (
-		<Fragment>
-			<Header>
-				<div className="logo" />
-				<Menu
-					theme="dark"
-					mode="horizontal"
-					defaultSelectedKeys={['1']}
-					style={{ lineHeight: '64px' }}>
-					<Menu.Item key="1">
-						<NavLink to="/">Home</NavLink>
-					</Menu.Item>
-					<Menu.Item key="2">
-						<NavLink to="/leaderboard">Leaderboard</NavLink>
-					</Menu.Item>
-					<Menu.Item key="3">
-						<NavLink to="/add">Add Question</NavLink>
-					</Menu.Item>
+	componentWillReceiveProps() {
+		this.handleClick();
+	}
 
-					{loggedIn ? (
-						<Menu.Item
-							key="5"
-							style={{
-								float: 'right',
-								marginRight: '50px',
-							}}>
-							<Avatar
-								size="small"
-								src={avatar}
-								style={{ marginRight: '10px' }}
-							/>
-							{authedUser} {' | '}
-							<NavLink
-								to="/login"
-								exact
-								style={{ display: 'inline' }}>
-								Logout
-							</NavLink>
+	render() {
+		const { authedUser, users } = this.props;
+		const avatar = authedUser
+			? users[authedUser].avatarURL
+			: 'https://i.imgur.com/dGo8DOk.jpg';
+
+		const loggedIn = authedUser !== null;
+
+		return (
+			<Fragment>
+				<Header>
+					<div className="logo" />
+					<Menu
+						theme="dark"
+						mode="horizontal"
+						onClick={this.handleClick}
+						selectedKeys={[this.state.current]}
+						style={{ lineHeight: '64px' }}>
+						<Menu.Item key="/">
+							<NavLink to="/">Home</NavLink>
 						</Menu.Item>
-					) : (
-						<Menu.Item
-							key="4"
-							style={{
-								float: 'right',
-								marginRight: '100px',
-							}}>
-							<NavLink to="/login" exact>
-								Login
-							</NavLink>
+						<Menu.Item key="/leaderboard">
+							<NavLink to="/leaderboard">Leaderboard</NavLink>
 						</Menu.Item>
-					)}
-				</Menu>
-			</Header>
-		</Fragment>
-	);
-};
+						<Menu.Item key="/add">
+							<NavLink to="/add">Add Question</NavLink>
+						</Menu.Item>
+
+						{loggedIn ? (
+							<Menu.Item
+								key="5"
+								style={{
+									float: 'right',
+									marginRight: '50px',
+								}}>
+								<Avatar
+									size="small"
+									src={avatar}
+									style={{ marginRight: '10px' }}
+								/>
+								{authedUser} {' | '}
+								<NavLink
+									to="/login"
+									exact
+									style={{ display: 'inline' }}>
+									Logout
+								</NavLink>
+							</Menu.Item>
+						) : (
+							<Menu.Item
+								key="4"
+								style={{
+									float: 'right',
+									marginRight: '100px',
+								}}>
+								<NavLink to="/login" exact>
+									Login
+								</NavLink>
+							</Menu.Item>
+						)}
+					</Menu>
+				</Header>
+			</Fragment>
+		);
+	}
+}
 
 const mapStateToProps = ({ authedUser, users }) => {
 	return {
