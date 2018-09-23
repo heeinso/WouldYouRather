9fcd6795827188cc478daba18a09475ffb73649f
heeinso <heeinso@snu.ac.kr>
2018-09-23 11:45:17 +0900

    Implement QuestionDetailPage component

--- src/components/QuestionDetailPage.js
+++ src/components/QuestionDetailPage.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import QuestionCard from './QuestionCard';
+import { Card, Row, Col } from 'antd';
 
-const QuestionDetailPage = () => {
-	return <div>QuestionPage</div>;
+import '../App.css';
+
+const QuestionDetailPage = props => {
+	const { questions, id } = props;
+
+	const question = questions[id];
+
+	return (
+		<div>
+			<h3
+				style={{
+					textAlign: 'center',
+					fontSize: '24px',
+					fontWeight: 'bold',
+					margin: '20px auto',
+				}}>
+				Question Detail
+			</h3>
+			<div
+				style={{
+					textAlign: 'left',
+					width: '350px',
+					margin: '50px auto',
+				}}>
+				{question && <QuestionCard question={question} />}
+			</div>
+		</div>
+	);
 };
 
 const mapStateToProps = ({ authedUser, questions }, props) => {
