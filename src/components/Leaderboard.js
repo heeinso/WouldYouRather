37247892f29015c7ab69e5e9f45ac2a20bab9930
heeinso <heeinso@snu.ac.kr>
2018-09-24 11:42:11 +0900

    Implement a LeaderBoard component

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,7 +1,63 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Avatar, Card, List } from 'antd';
 
-const Leaderboard = () => {
-	return <div>Leaderboard</div>;
+const { Meta } = Card;
+
+const Leaderboard = props => {
+	const { users } = props;
+	const sorting = Object.values(users).sort((a, b) => {
+		const sumA = Object.keys(a.answers).length + a.questions.length;
+		const sumB = Object.keys(b.answers).length + b.questions.length;
+		return sumB - sumA;
+	});
+
+	return (
+		<div>
+			<h3
+				style={{
+					textAlign: 'center',
+					fontSize: '24px',
+					fontWeight: 'bold',
+					margin: '20px auto',
+				}}>
+				Ranking
+			</h3>
+			<List
+				grid={{ gutter: 16, column: 1 }}
+				dataSource={sorting}
+				renderItem={user => {
+					const content = `Asked: ${
+						user.questions.length
+					}, Answered: ${Object.keys(user.answers).length}`;
+					return (
+						<List.Item key={user.id}>
+							<div
+								style={{
+									textAlign: 'left',
+									width: '350px',
+									margin: '0 auto',
+								}}>
+								<Card>
+									<Meta
+										avatar={<Avatar src={user.avatarURL} />}
+										title={user.name}
+										description={content}
+									/>
+								</Card>
+							</div>
+						</List.Item>
+					);
+				}}
+			/>
+		</div>
+	);
 };
 
-export default Leaderboard;
+const mapStateToProps = ({ users }) => {
+	return {
+		users,
+	};
+};
+
+export default connect(mapStateToProps)(Leaderboard);
